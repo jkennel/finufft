4d8653d559680fb98728aaaa02aae2de2659d8ad
Jonathan Kennel <jonathankennel@pop-os.localdomain>
2020-10-15 14:42:53 -0400

    test stub

--- tests/testthat/test-nfft.R
+++ tests/testthat/test-nfft.R
@@ -10,10 +10,6 @@ test_that("nfft 1-D works", {
   y <- complex(real = sin(3000 * x) + sin(200 * x)*0.2, imaginary = 0)
   
   nfft_res <- nfft(x, y, 10000)
+  testthat::expect_equal(length(nfft_res), 10000)
   
-  # fft_res <- fft(y_comp)
-  # 
-  # plot(seq(1, n, 10), sqrt(Re(nfft_res * Conj(nfft_res)))/length(s),  type = 'l')
-  # points(sqrt(Re(fft_res * Conj(fft_res)))/(n), type = 'l', col = 'red')
-  # 
 })
